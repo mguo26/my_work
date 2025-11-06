8e3da5e55c38af6ae4574bb0fbd527ec5bbaf459
mia guo <mguo26@nmhschool.org>
2025-11-05 20:31:44 -0500

    Update app.py
    homework 2

--- iteration_05/app.py
+++ iteration_05/app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template, request
-import jinja2
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
-
-@app.route("/contact")
-def contact():
-    contact = {"email": "mguo26@nmhschool.org", "phone": None, "github": "mguo26/my_work"}
-    return render_template("contact.html", contact=contact)
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html", name="Mia", interest="AI")
+
+@app.route("/contact")
+def contact():
+    contact = {"email": "mguo26@nmhschool.org", "github": "mguo26/my_work", "phone": None}
+    return render_template("contact.html", contact=contact)
 
 if __name__ == "__main__":
     app.run(debug=True)
